8efd37198beceb144c7f2330143dcb8f0bef8da4
sadekovlar <89263920392@mail.ru>
2023-10-08 10:59:21 +0300

    Навел порядок в репозитории

--- main.py
+++ main.py
@@ -4,42 +4,25 @@ import os
 import glob
 import numpy as np
 import sys
+    
 
-def main():
+if __name__ == "__main__":
+    csv_files = glob.glob(os.path.join("./data", "*.tsv"))
 
-    path = os.getcwd()
-    csv_files = glob.glob(os.path.join(path, "*.tsv"))
-    dfs = []
-
+    dfs = dict()
     for f  in csv_files:
-        df = pd.read_csv(f, sep='\t')
-        dfs.append(df)
+        df = pd.read_csv(f, sep='\t',skiprows=[1])
+        dfs[f] = df
 
     fig = go.Figure()
-    color = 0
-
-    for i in dfs:
-
-        size = i.size
-        zs = np.zeros(size)
-        color = np.random.randint(int(0xFFFFFF))
-    
+    color = 125
+    for name in dfs.keys():
+        data = dfs[name]
         fig.add_trace(go.Scatter3d(
-            x = i['lat[deg]'],
-            y = i['long[deg]'],
-            z = i['path_meter_pos[m]'], 
+            x = data['lat[deg]'],
+            y = data['long[deg]'],
+            z = data['speed[kmph]'], 
             line=dict(color=str('#%06x' % (color))),
             marker=dict(size=1)
         ))
-        fig.add_trace(go.Scatter3d(
-            x = i['path_meter_pos[m]'],
-            y = i['speed[kmph]'],
-            z = zs, 
-            line=dict(color=str('#%06x' % (color))),
-            marker=dict(size=1)
-        ))
-
     fig.show()
-    
-if __name__ == "__main__":
-    main()
