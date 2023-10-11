ee8c943aa2e92aea66831452ce74687e2a13bf72
Rinat <89263920392@mail.ru>
2023-10-11 12:57:00 +0300

    Добавил данные для визуализации

--- main.py
+++ main.py
@@ -3,37 +3,35 @@ import pandas as pd
 import os
 import glob
 import numpy as np
-import sys
-    
+
 
 if __name__ == "__main__":
-    csv_files = glob.glob(os.path.join("./data", "*.tsv"))
+    csv_files = glob.glob(os.path.join("./data", "*extention.tsv"))
 
     dfs = dict()
     for f  in csv_files:
-        df = pd.read_csv(f, sep='\t',skiprows=[1])
+        df = pd.read_csv(f, sep='\t', skiprows=[1])
         dfs[f] = df
 
     fig = go.Figure()
-    color = 125
     for name in dfs.keys():
         data = dfs[name]
         zs = np.zeros(data.size)
         fig.add_trace(go.Scatter3d(
-            x = data['lat[deg]'],
-            y = data['long[deg]'],
-            z = data['speed[kmph]'], 
-            line=dict(color=str('#%06x' % (color))),
+            x=data['lat[deg]'],
+            y=data['long[deg]'],
+            z=data['speed[kmph]'],
+            mode='lines',
+            line=dict(color="rgb(100, 100, 100)"),
             marker=dict(size=1)
         ))
         fig.add_trace(go.Scatter3d(
-            x = data['lat[deg]'],
-            y = data['long[deg]'],
-            z = zs, 
-            line=dict(color=str('#%06x' % (color))),
+            x=data['lat[deg]'],
+            y=data['long[deg]'],
+            z=zs,
+            mode='lines',
+            line=dict(color="rgb(128, 64, 48)"),
             marker=dict(size=1)
-        ))
-    
+        ))  
     fig.update_scenes(aspectratio=dict(x=2, y=2, z=1))
-    
     fig.show()
